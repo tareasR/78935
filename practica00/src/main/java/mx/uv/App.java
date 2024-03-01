2e8ccc3c77d1f9b1740cbd6fa862303dfafb5512
tareasr <tareasrojano@gmail.com>
2024-03-01 16:51:38 -0600

    practica00

--- practica00/src/main/java/mx/uv/App.java
+++ practica00/src/main/java/mx/uv/App.java
@@ -1,5 +1,6 @@
 package mx.uv;
 
+import static spark.Spark.*;
 /**
  * Hello world!
  *
@@ -9,5 +10,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        get("/xml", (request, response) -> {
+            String respuesta = "<x><y/></x>";
+            response.type("application/xml");
+            return respuesta;
+        });
     }
 }
