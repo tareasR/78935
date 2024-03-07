7f0a1af7718057227d3860be79908bd0419c0bd4
tareasr <tareasrojano@gmail.com>
2024-03-07 15:31:48 -0600

    cors

--- practica00/src/main/java/mx/uv/App.java
+++ practica00/src/main/java/mx/uv/App.java
@@ -1,6 +1,7 @@
 package mx.uv;
 
 import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -11,8 +12,34 @@ public class App
     {
         System.out.println( "Hello World!" );
 
-        get("/xml", (request, response) -> {
-            String respuesta = "<x><y/></x>";
+        // para permitir el CORS
+        //fuente:https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
+        options("/*",(request,response)->{
+        String accessControlRequestHeaders=request.headers("Access-Control-Request-Headers");
+        if(accessControlRequestHeaders!=null){
+        response.header("Access-Control-Allow-Headers",accessControlRequestHeaders);
+        }
+        String accessControlRequestMethod=request.headers("Access-Control-Request-Method");
+        if(accessControlRequestMethod!=null){
+        response.header("Access-Control-Allow-Methods",accessControlRequestMethod);
+        }
+        return "OK";
+        });
+        before((request,response)->response.header("Access-Control-Allow-Origin","*"));
+
+
+        // (pr1, pr2) -> "";
+        // (pr1, pr2) -> {return null;};
+        get("/", (request, response)-> "hola");
+
+        get("/xml", (request, response)-> {
+            String respuesta=
+            "<p>" + 
+                "<x>" + 
+                    "<y>algo</y>" + 
+                "</x>" + 
+                "<x><y>algo2</y></x>" + 
+            "</p>";
             response.type("application/xml");
             return respuesta;
         });
