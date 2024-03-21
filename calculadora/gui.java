5e473c5a1cea0a9ef7a7ee90dcbe2ed6ab4401ad
tareasr <tareasrojano@gmail.com>
2024-03-21 16:53:14 -0600

    axis example complete

---
+++ calculadora/gui.java
@@ -0,0 +1,7 @@
+public class gui {
+    public static void main(String[] args) {
+        System.out.println("hola");
+        calculadora c = new calculadora();
+        System.out.println(c.suma(2,3));
+    }
+}
